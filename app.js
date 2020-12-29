3373e50e8ed79edc05a536e66b9db5f22611c402
Beck Anderson <bea1935@rit.edu>
2020-12-28 23:14:07 -0500

    test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Finally Got it'));
+app.get('/', (req, res) => res.send('Finally Got it (pt 2)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
